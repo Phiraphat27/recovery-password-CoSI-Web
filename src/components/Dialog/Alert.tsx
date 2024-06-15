05ec630c9b40246bdd2ffe4e1389924439bd9e8d
ponlawat khamlae <ponlawat.kham@gmail.com>
2024-06-15 16:38:23 +0700

    feat: Add DialogCustomAnimation component
    The code changes add a new component called DialogCustomAnimation. This component is responsible for rendering a dialog with custom animation. It takes props for controlling the open status, message, and additional info. The dialog includes a header, body, and footer with buttons for confirming or canceling the action.

---
+++ src/components/Dialog/Alert.tsx
@@ -0,0 +1,54 @@
+// components/DialogCustomAnimation.tsx
+
+import React, { Dispatch, SetStateAction, useEffect } from "react";
+import {
+    Button,
+    Dialog,
+    DialogHeader,
+    DialogBody,
+    DialogFooter,
+} from "@material-tailwind/react";
+
+export default function DialogCustomAnimation({ open = { status: false, message: "" } , setOpen, info }: {
+    open?:{
+        status: boolean;
+        message: string;
+    }, info?: string, setOpen: Dispatch<SetStateAction<{
+        status: boolean;
+        message: string;
+    }>>
+}) {
+
+    const handleOpen = () => setOpen({ status: false, message: "" });
+
+    return (
+        <>
+            <Dialog
+                open={open.status}
+                size="xs"
+                className="dark:bg-black dark:text-gray-200 border dark:border-gray-200"
+                handler={handleOpen}
+                animate={{
+                    mount: { scale: 1, y: 0 },
+                    unmount: { scale: 0.9, y: -100 },
+                }} placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}            >
+                <DialogHeader placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>It's a simple dialog.</DialogHeader>
+                <DialogBody placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+                    {info}
+                </DialogBody>
+                <DialogFooter placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+                    {/* <Button
+                        variant="text"
+                        color="red"
+                        onClick={handleOpen}
+                        className="mr-1" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}                    >
+                        <span>Cancel</span>
+                    </Button> */}
+                    <Button variant="gradient" color="green" onClick={handleOpen} placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+                        <span>Confirm</span>
+                    </Button>
+                </DialogFooter>
+            </Dialog>
+        </>
+    );
+}
