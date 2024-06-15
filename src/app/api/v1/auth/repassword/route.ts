154cd5dca46aedede3549ee352a669196c6572a0
ponlawat khamlae <ponlawat.kham@gmail.com>
2024-06-15 16:37:55 +0700

    feat: Add route for resetting password
    The code changes add a new file `route.ts` in the `src/app/api/v1/auth/repassword` directory. This file contains a `POST` route handler that handles the request for resetting a password. It uses the `forgotPassword` function from the `@/lib/auth` module to reset the password for a given email. If the email is valid, it sends a success response with a status code of 200 and a message of "Email sent". If the email is invalid, it sends an error response with a status code of 401 and a message of "Invalid credentials".
    
    This commit adds the necessary code to handle password reset functionality.

---
+++ src/app/api/v1/auth/repassword/route.ts
@@ -0,0 +1,12 @@
+import { forgotPassword } from "@/lib/auth";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+    const { email, password } = await req.json();
+    if (email) {
+        const user = await forgotPassword(email, password);
+        return NextResponse.json({ "message": "Email sent" }, { status: 200 });
+    } else {
+        return NextResponse.json({ "message": "Invalid credentials" }, { status: 401 });
+    }
+}
