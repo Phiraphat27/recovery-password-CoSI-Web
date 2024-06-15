061df40a5c25f033d3d98b1cc96a9297c9a5d769
ponlawat khamlae <ponlawat.kham@gmail.com>
2024-06-15 18:11:13 +0700

    refactor: Update sessionData type to include additional user details
    The code changes in the `sessionData.ts` file update the `sessionData` type to include additional user details such as `user_password`, `user_image`, `permission`, `position`, and `profile`. This change allows for more comprehensive session data retrieval and improves the functionality of the application.

--- src/type/sessionData.ts
+++ src/type/sessionData.ts
@@ -1,7 +1,23 @@
-export interface sessionData {
-    user_id: string;
-    user_name: string;
-    user_email: string;
-    user_position: string | null;
-    user_permission: string | null;
-}
+import { Prisma } from "@prisma/client"
+
+export type sessionData = Prisma.userGetPayload<{
+    select: {
+        user_id: true,
+        user_email: true,
+        user_password: true,
+        user_name: true,
+        user_image: true,
+        permission: {
+            select: { permission_name: true }
+        },
+        position: {
+            select: { position_name: true }
+        },
+        profile: {
+            select: {
+                name: true,
+                language_code: true,
+            }
+        }
+    }
+}>
