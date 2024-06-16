2de2e1771d13346cda85577c007116ecd4f0748d
ponlawat khamlae <ponlawat.kham@gmail.com>
2024-06-16 16:03:21 +0700

    refactor: Update TabsWithIcon component in page.tsx
    The code changes in the `page.tsx` file update the `TabsWithIcon` component. The labels, values, and icons for the tabs are modified to improve the user interface and provide more meaningful options. This refactor enhances the functionality and visual consistency of the page.

--- src/app/[locale]/office/setting/page.tsx
+++ src/app/[locale]/office/setting/page.tsx
@@ -11,29 +11,30 @@ import {
     Square3Stack3DIcon,
     UserCircleIcon,
     Cog6ToothIcon,
+    FingerPrintIcon
 } from "@heroicons/react/24/solid";
 
 export function TabsWithIcon() {
     const data = [
         {
-            label: "Dashboard",
-            value: "dashboard",
-            icon: Square3Stack3DIcon,
+            label: "Accounts",
+            value: "accounts",
+            icon: UserCircleIcon,
             desc: `It really matters and then like it really doesn't matter.
       What matters is the people who are sparked by it. And the people
       who are like offended by it, it doesn't matter.`,
         },
         {
-            label: "Profile",
-            value: "profile",
-            icon: UserCircleIcon,
+            label: "Two-factor",
+            value: "twoFactor",
+            icon: FingerPrintIcon,
             desc: `Because it's about motivating the doers. Because I'm here
       to follow my dreams and inspire other people to follow their dreams, too.`,
         },
         {
-            label: "Settings",
-            value: "settings",
-            icon: Cog6ToothIcon,
+            label: "Sessions",
+            value: "sessions",
+            icon: Square3Stack3DIcon,
             desc: `We're not always in the position that we want to be at.
       We're constantly growing. We're constantly making mistakes. We're
       constantly trying to express ourselves and actualize our dreams.`,
@@ -42,12 +43,13 @@ export function TabsWithIcon() {
     return (
         <div className="max-w-7xl mx-auto dark:text-white">
             <Tabs value="dashboard">
-                <TabsHeader className="dark:text-white dark:border-white dark:border" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+                <TabsHeader className=" bg-gray-200 dark:bg-transparent dark:text-white dark:border-white dark:border" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
                     {data.map(({ label, value, icon }) => (
                         <Tab
                             key={value}
                             value={value}
-                            className="dark:text-gray-700" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}                        >
+                            className="dark:text-gray-700" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}
+                            >
                             <div className="flex items-center gap-2">
                                 {React.createElement(icon, { className: "w-5 h-5" })}
                                 {label}
