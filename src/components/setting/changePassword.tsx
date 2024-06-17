9806ce46d301511cae7e7c24c40d56d73741817f
ponlawat khamlae <ponlawat.kham@gmail.com>
2024-06-17 17:38:54 +0700

    refactor: Update font color and password handling in ChangePassword component

--- src/components/setting/changePassword.tsx
+++ src/components/setting/changePassword.tsx
@@ -77,7 +77,7 @@ export default function ChangePassword() {
 
     return (
         <div className="flex justify-evenly flex-col md:flex-row">
-            <div className="w-full max-w-sm">
+            <div className="w-full max-w-lg">
                 <div className='font-semibold text-lg'>Change Password</div>
                 <div className='font-medium text-md text-gray-500 dark:text-gray-200'>Update your profile information below.</div>
                 <div className="pt-6 mb-1 flex flex-col gap-6">
@@ -131,7 +131,7 @@ export default function ChangePassword() {
                     </Button>
                 </div>
             </div>
-            <div className="w-full max-w-sm pt-4 md:pt-0">
+            <div className="w-full max-w-md pt-4 md:pt-0">
                 <div className='font-semibold text-lg'>Password Requirement </div>
                 <div className='font-medium text-md text-gray-500 dark:text-gray-200'>Please follow this guide for a strong password: </div>
                 <div className='pt-6 font-normal text-sm'>
